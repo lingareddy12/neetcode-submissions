8d9a6b20a210e516b882663608fa666174e82a9b
LINGA REDDY DANDA <111686775+lingareddy12@users.noreply.github.com>
2026-08-09 13:25:55 +0530

    Add: longest-common-prefix - submission-8

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        first_str=strs[0]
+        for i in range(len(first_str)): 
+            for string in strs[1:]:
+                if i==len(string) or string[i]!=first_str[i]:
+                    return first_str[:i]
+
+        return first_str
+                
+
+        
